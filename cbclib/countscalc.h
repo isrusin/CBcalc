188160f4547200903750546d232a5923386b37db
isrusin <10874317+isrusin@users.noreply.github.com>
2024-08-03 17:52:42 +0300

    Fix treatment of errors in counts module.
    * Correct error-related variable definitios.
    * Check fasta fd, closes #2.

--- cbclib/countscalc.h
+++ cbclib/countscalc.h
@@ -5,10 +5,11 @@
 #include <stdlib.h>
 #include <zlib.h>
 
-int error_type;
-const int FILE_ERROR;
-const int MEMORY_ERROR;
-char *error_message;
+#define FILE_ERROR 1
+#define MEMORY_ERROR 2
+
+extern int error_type;
+extern char *error_message;
 
 long **count_short_words(char *, int);
 long **count_bipart_words(char *, int, int, int);
